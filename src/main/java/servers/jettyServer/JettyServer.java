95f04424f754aabd5d07a7e460cf335ead0eaa77
ruchamaslekar <ruchamaslekar@gmail.com>
2023-11-06 19:06:33 -0800

    Added login in JettyServer as below: 1.Initializing ServletContextHandler 2.Calling addServlet method inside addMapping method 3.Calling setAttribute method inside setResourceAttribute method 4.Calling start and join methods in start method

--- src/main/java/servers/jettyServer/JettyServer.java
+++ src/main/java/servers/jettyServer/JettyServer.java
@@ -1,34 +1,36 @@
 package servers.jettyServer;
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 
 /** This class uses Jetty & servlets to implement server responding to Http GET requests */
 public class JettyServer {
     // FILL IN CODE
     private static final int PORT = 8090;
-    private Server jettyServer;  // Jetty server
-    private ServletHandler handler; // handler for the jetty server
+    private final Server jettyServer;  // Jetty server
+    private final ServletContextHandler handlers = new ServletContextHandler(ServletContextHandler.SESSIONS); //handler for the jetty server
+
     // FILL IN CODE: add other variables and methods as needed:
     // store a resource/resources you need to pass to the servlets
     // (such as ThreadSafeHotelData, but you want to store resources as objects to make the code more general).
-
     public JettyServer() {
         jettyServer = new Server(PORT);
-        handler = new ServletHandler();
-        jettyServer.setHandler(handler);
+        jettyServer.setHandler(handlers);
     }
     /**
      * Maps a given URL path/endpoint to the name of the servlet class that will handle requests coming at this endpoint
      * @param path end point
      * @param className  name of the servlet class
      */
-    private void addMapping(String path, String className){
+    public void addMapping(String path, Class className) {
         // FILL IN CODE: call the method on the handler that adds the mapping between the path and the servlet
-
+         handlers.addServlet(className, path);
     }
 
     // FILL IN CODE: add other methods as needed
+    public void setResourceAttribute(String resourceName, Object resource) {
+        handlers.setAttribute(resourceName, resource);
+    }
 
     /**
      * Function that starts the server
@@ -36,6 +38,48 @@ public class JettyServer {
      */
     public  void start() throws Exception {
         // FILL IN CODE: run the jetty server
+        try {
+            jettyServer.start();
+            jettyServer.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//        Server server = new Server(PORT);
+//        ServletContextHandler handlers= new ServletContextHandler();
+//        handlers.addServlet(HotelServlet.class, "/hotelInfo");
+//        handlers.addServlet(ReviewServlet.class, "/reviewInfo");
+//        handlers.addServlet(InvertedIndexServlet.class, "/wordInfo");
+//        handlers.setAttribute("hotel",resources.get("hotel"));
+//        handlers.setAttribute("review",reviewDetails);
+//        handlers.setAttribute("word",invertedIndex);
+//        server.setHandler(handlers);
+//        server.start();
+//        server.join();
+//        addMapping("/hotelInfo", "HotelServlet");
+//        addMapping("/reviewInfo", "ReviewServlet");
+//        addMapping("/wordInfo", "InvertedIndexServlet");
+
+
+        /**Hotel data*/
+//        HotelParser parser = new HotelParser();
+//        HotelDetails hotelDetails = new HotelDetails();
+//        handlers.addServlet(HotelServlet.class, "/hotelInfo");
+//        parser.parseHotelJson("/Users/ruchamaslekar/Software Development/project4-ruchamaslekar/input/hotels/hotels.json",hotelDetails);
+//        server.setHandler(handlers);
+//        handlers.setAttribute("hotel",hotelDetails);
+
+        /**Review data*/
+//        ExecutorService poolManager = Executors.newFixedThreadPool(3);
+//        MultithreadedDirectoryParser parser1 = new MultithreadedDirectoryParser();
+//        HotelParser parser = new HotelParser();
+//        HotelDetails hotelDetails = new ThreadSafeHotelDetails();
+//        parser.parseHotelJson("/Users/ruchamaslekar/Software Development/project4-ruchamaslekar/input/hotels/hotels.json",hotelDetails);
+//        ReviewDetails reviewDetails = new ThreadSafeReviewDetails();
+//        handlers.addServlet(ReviewServlet.class, "/reviewInfo");
+//        parser1.recursivelyParseDirectory("/Users/ruchamaslekar/Software Development/project4-ruchamaslekar/input/reviews",reviewDetails,poolManager);
+//        server.setHandler(handlers);
+//        handlers.setAttribute("review",reviewDetails);
 
     }
 }
