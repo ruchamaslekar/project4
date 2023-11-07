138954ba6bca39261de575feaaed4c1082122354
ruchamaslekar <ruchamaslekar@gmail.com>
2023-11-06 19:02:33 -0800

    Modified logic in JettyServerDriver as below: 1.Parsing hotel,review and invertedIndex classes and adding data to map in an object 2.Calling addMapping method by passing parameters as path and class 3.Calling setResourceAttribute method with parameter resourceName and the map object 4.Calling start method in JettyServer

--- src/main/java/servers/JettyServerDriver.java
+++ src/main/java/servers/JettyServerDriver.java
@@ -1,9 +1,28 @@
 package servers;
 
+import hotelData.HotelDetails;
+import hotelData.HotelParser;
+import hotelData.ThreadSafeHotelDetails;
+import invertedIndexData.InvertedIndex;
+import invertedIndexData.InvertedIndexParser;
+import invertedIndexData.ThreadSafeInvertedIndex;
+import reviewData.MultithreadedDirectoryParser;
+import reviewData.ReviewDetails;
+import reviewData.ThreadSafeReviewDetails;
+import servers.jettyServer.HotelServlet;
+import servers.jettyServer.InvertedIndexServlet;
+import servers.jettyServer.JettyServer;
+import servers.jettyServer.ReviewServlet;
+
+import javax.servlet.http.HttpServlet;
+import java.util.ArrayList;
+import java.util.List;
+
+
 /** Driver class for running the Jetty server.
  * Create a jar file from this class.  */
 public class JettyServerDriver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // FILL IN CODE:
         // Unlike the servers, the drivers will be specific to the hotel/reviews project.
 
@@ -12,5 +31,38 @@ public class JettyServerDriver {
         // Create the JettyServer, call addMapping for the endpoints to map them to servlets,
         // pass ThreadSafeHotelData etc as the resource,
         // then start the server
+        ProgramArgumentParser programParser = new ProgramArgumentParser();
+        programParser.parseArgs(args);
+
+        /**HotelParsing*/
+        HotelParser parser = new HotelParser();
+        HotelDetails hotelDetails = new ThreadSafeHotelDetails();
+        parser.parseHotelJson(programParser.getArgument("-hotels"),hotelDetails);
+
+        /**ReviewParsing*/
+        MultithreadedDirectoryParser directoryParser = new MultithreadedDirectoryParser();
+        ReviewDetails reviewDetails = new ThreadSafeReviewDetails();
+        directoryParser.parseDirectory(programParser, reviewDetails, hotelDetails);
+
+        /**InvertedIndexParsing*/
+        InvertedIndex invertedIndex = new ThreadSafeInvertedIndex();
+        InvertedIndexParser indexParser = new InvertedIndexParser();
+        indexParser.ParseDirectory(programParser.getArgument("-reviews"),invertedIndex,reviewDetails);
+
+        /** JettyServer */
+        JettyServer server = new JettyServer();
+
+        /** Setting resources map */
+        server.addMapping("/hotelInfo", HotelServlet.class);
+        server.addMapping("/reviewInfo", ReviewServlet.class);
+        server.addMapping("/wordInfo", InvertedIndexServlet.class);
+
+        /** Setting resources attributes */
+        server.setResourceAttribute("hotel", hotelDetails);
+        server.setResourceAttribute("review", reviewDetails);
+        server.setResourceAttribute("word", invertedIndex);
+
+        server.start();
+
     }
 }
