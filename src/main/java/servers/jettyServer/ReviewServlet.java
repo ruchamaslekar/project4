f8071105cc72eb1d9ff4e3ddbe6c4d9d01906ca2
ruchamaslekar <ruchamaslekar@gmail.com>
2023-11-13 19:19:25 -0800

    Modified ReviewServlet class to pass the test cases

--- src/main/java/servers/jettyServer/ReviewServlet.java
+++ src/main/java/servers/jettyServer/ReviewServlet.java
@@ -1,57 +1,70 @@
 package servers.jettyServer;
 
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import org.apache.commons.text.StringEscapeUtils;
 import reviewData.Review;
 import reviewData.ReviewDetails;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
 public class ReviewServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("application/json");
-        response.setStatus(HttpServletResponse.SC_OK);
-        PrintWriter out = response.getWriter();
-        String hotelId = request.getParameter("hotelId");
-        int numOfReviews = (Integer.parseInt(request.getParameter("num"))) ;
-        hotelId = StringEscapeUtils.escapeHtml4(hotelId);
-        JsonArray array = new JsonArray();
-        JsonObject responseJson = new JsonObject();
-        JsonObject responseJson1 = new JsonObject();
-        if(hotelId != null && !hotelId.isEmpty()) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_OK);
+            PrintWriter out = response.getWriter();
+            JsonObject responseJson = new JsonObject();
             ReviewDetails reviewDetails = (ReviewDetails) getServletContext().getAttribute("review");
-            List<Review> reviews = reviewDetails.getReviews(hotelId,numOfReviews);
-            if(reviews == null ){
-                responseJson.addProperty("success",false);
-                responseJson.addProperty("hotelId","invalid");
+            String hotelId = request.getParameter("hotelId");
+            hotelId = StringEscapeUtils.escapeHtml4(hotelId);
+            if (hotelId == null ) {
+                responseJson.addProperty("hotelId", "invalid");
+                responseJson.addProperty("success", false);
+                response.getWriter().println(responseJson);
+                out.flush();
+            } else if(request.getParameter("num")== null || request.getParameter("num" ).isEmpty()){
+                responseJson.addProperty("num", "invalid");
+                responseJson.addProperty("success", false);
+                response.getWriter().println(responseJson);
+                out.flush();
             }
-            else{
-                responseJson.addProperty("success","true");
-                responseJson.addProperty("hotelId",hotelId);
-                for(Review review: reviews){
-                    responseJson1.addProperty("reviewId",review.getReviewId());
-                    responseJson1.addProperty("reviewText",review.getReviewText());
-                    responseJson1.addProperty("rating",review.getRatingOverall());
-                    array.add(responseJson1);
-                    responseJson.add("reviews", array);
+            else {
+                int numOfReviews = (Integer.parseInt(request.getParameter("num")));
+                List<Review> reviews = reviewDetails.getReviews(hotelId, numOfReviews);
+                if (reviews.isEmpty()) {
+                    responseJson.addProperty("hotelId", "invalid");
+                    responseJson.addProperty("success", false);
+                }else {
+                    JsonArray array = new JsonArray();
+                    responseJson.addProperty("hotelId", hotelId);
+                    for (Review review : reviews) {
+                        if(review!= null) {
+                            JsonObject reviewJson = new JsonObject();
+                            reviewJson.addProperty("user", review.getUserNickname());
+                            reviewJson.addProperty("title", review.getTitle());
+                            reviewJson.addProperty("reviewId", review.getReviewId());
+                            reviewJson.addProperty("reviewText", review.getReviewText());
+                            array.add(reviewJson);
+                            responseJson.add("reviews", array);
+
+                        }else {
+                            responseJson.addProperty("success", false);
+                            responseJson.addProperty("hotelId", "invalid");
+                        }
+                    }
+                    responseJson.addProperty("success", true);
                 }
-
+                response.getWriter().println(responseJson);
+                out.flush();
             }
-        } else{
-            responseJson.addProperty("success:",false);
-            responseJson.addProperty("hotelId:","invalid");
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        out.println(responseJson);
-        out.flush();
     }
-
 }
