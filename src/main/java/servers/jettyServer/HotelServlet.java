11e58664efc0804a6e204a7504f57239385fd59c
ruchamaslekar <ruchamaslekar@gmail.com>
2023-11-13 19:19:08 -0800

    Modified HotelServlet class to pass the test cases

--- src/main/java/servers/jettyServer/HotelServlet.java
+++ src/main/java/servers/jettyServer/HotelServlet.java
@@ -4,48 +4,51 @@ import com.google.gson.JsonObject;
 import hotelData.Hotel;
 import hotelData.HotelDetails;
 import org.apache.commons.text.StringEscapeUtils;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.PrintWriter;
 
 public class HotelServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("application/json");
-        response.setStatus(HttpServletResponse.SC_OK);
-        PrintWriter out = response.getWriter();
-        String hotelId = request.getParameter("hotelId");
-        hotelId = StringEscapeUtils.escapeHtml4(hotelId);
-        JsonObject responseJson = new JsonObject();
-        if (hotelId != null && !hotelId.isEmpty()) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try{
+            response.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_OK);
+            PrintWriter out = response.getWriter();
+            JsonObject responseJson = new JsonObject();
             HotelDetails hotelDetails = (HotelDetails) getServletContext().getAttribute("hotel");
-            Hotel hotel  = hotelDetails.getHotel(hotelId);
-            if(hotel == null ){
-                responseJson.addProperty("success",false);
-                responseJson.addProperty("hotelId","invalid");
-                out.println(responseJson);
+            String hotelId = request.getParameter("hotelId");
+            hotelId = StringEscapeUtils.escapeHtml4(hotelId);
+            if(hotelId == null){
+                responseJson.addProperty("hotelId", "invalid");
+                responseJson.addProperty("success", false);
+                response.getWriter().println(responseJson);
+                out.flush();
             }
             else {
-                responseJson.addProperty("success","true");
-                responseJson.addProperty("hotelId",hotelId);
-                responseJson.addProperty("name",hotel.getHotelName());
-                responseJson.addProperty("addr",hotel.getAddress());
-                responseJson.addProperty("city",hotel.getCity());
-                responseJson.addProperty("lat",hotel.getLatitude());
-                responseJson.addProperty("lng",hotel.getLongitude());
-                out.println(responseJson.toString());
+                Hotel hotel  = hotelDetails.getHotel(hotelId);
+                if (hotel != null) {
+                    responseJson.addProperty("hotelId", hotelId);
+                    responseJson.addProperty("name", hotel.getHotelName());
+                    responseJson.addProperty("addr", hotel.getAddress());
+                    responseJson.addProperty("city", hotel.getCity());
+                    responseJson.addProperty("state", hotel.getState());
+                    responseJson.addProperty("lat", hotel.getLatitude());
+                    responseJson.addProperty("lng", hotel.getLongitude());
+                    responseJson.addProperty("success", true);
+                    response.getWriter().println(responseJson);
+                    out.flush();
+                } else {
+                    responseJson.addProperty("hotelId", "invalid");
+                    responseJson.addProperty("success", false);
+                    response.getWriter().println(responseJson);
+                    out.flush();
+                }
             }
-            out.flush();
-        } else {
-            responseJson.addProperty("success",false);
-            responseJson.addProperty("hotelId","invalid");
-            out.println(responseJson);
-            out.flush();
+        } catch(Exception e) {
+            System.out.println(e);
         }
-
     }
 }
