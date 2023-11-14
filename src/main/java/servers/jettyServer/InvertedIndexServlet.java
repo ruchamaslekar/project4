d2b6a4975acfa65c3d6d0c65757cb0d97a72b7ef
ruchamaslekar <ruchamaslekar@gmail.com>
2023-11-13 19:18:48 -0800

    Modified InvertedIndexServlet class to pass the test cases

--- src/main/java/servers/jettyServer/InvertedIndexServlet.java
+++ src/main/java/servers/jettyServer/InvertedIndexServlet.java
@@ -5,7 +5,6 @@ import com.google.gson.JsonObject;
 import invertedIndexData.InvertedIndex;
 import invertedIndexData.ReviewFrequency;
 import org.apache.commons.text.StringEscapeUtils;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,43 +15,48 @@ import java.util.Set;
 public class InvertedIndexServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_OK);
+        JsonObject responseJson = new JsonObject();
         PrintWriter out = response.getWriter();
         String word = request.getParameter("word");
-        int numOfReviews = (Integer.parseInt(request.getParameter("num"))) ;
         word = StringEscapeUtils.escapeHtml4(word);
-        JsonArray array = new JsonArray();
-        JsonObject responseJson = new JsonObject();
-        JsonObject responseJson1 = new JsonObject();
-        if(word != null && !word.isEmpty()) {
+        if (word == null || word.isEmpty()) {
+            responseJson.addProperty("success", false);
+            responseJson.addProperty("word", "invalid");
+            response.getWriter().println(responseJson);
+            out.flush();
+        } else if (request.getParameter("num") == null || request.getParameter("num").isEmpty()) {
+            responseJson.addProperty("success", false);
+            responseJson.addProperty("num", "invalid");
+            response.getWriter().println(responseJson);
+            out.flush();
+        } else {
+            int numOfReviews = (Integer.parseInt(request.getParameter("num")));
+            JsonArray array = new JsonArray();
             InvertedIndex invertedIndex = (InvertedIndex) getServletContext().getAttribute("word");
-            Set<ReviewFrequency> reviews = invertedIndex.searchByWord(word,numOfReviews);
-            if(reviews == null){
-                responseJson.addProperty("success",false);
-                responseJson.addProperty("word","invalid");
-                out.println(responseJson);
-            }
-            else {
-                responseJson.addProperty("success",true);
-                responseJson.addProperty("word",word);
-                for (ReviewFrequency review : reviews) {
-                    responseJson1.addProperty("reviewId",review.getReview().getReviewId());
-                    responseJson1.addProperty("title",review.getReview().getTitle());
-                    responseJson1.addProperty("user",review.getReview().getUserNickname());
-                    responseJson1.addProperty("reviewText",review.getReview().getReviewText());
-                    responseJson1.addProperty("date",review.getReview().getDate().toString());
-                    array.add(responseJson1);
+            Set<ReviewFrequency> reviews = invertedIndex.searchByWord(word, numOfReviews);
+            if (reviews != null && !reviews.isEmpty()) {
+                ReviewFrequency[] setArray = reviews.toArray(new ReviewFrequency[0]);
+                responseJson.addProperty("word", word);
+                for(ReviewFrequency reviewFrequency : setArray) {
+                    JsonObject reviewJson = new JsonObject();
+                    reviewJson.addProperty("reviewId", reviewFrequency.getReview().getReviewId());
+                    reviewJson.addProperty("title", reviewFrequency.getReview().getTitle());
+                    reviewJson.addProperty("user", reviewFrequency.getReview().getUserNickname());
+                    reviewJson.addProperty("reviewText", reviewFrequency.getReview().getReviewText());
+                    reviewJson.addProperty("date", reviewFrequency.getReview().getDate().toString());
+                    array.add(reviewJson);
                     responseJson.add("reviews", array);
                 }
+                responseJson.addProperty("success", true);
+            } else {
+                responseJson.addProperty("success", false);
+                responseJson.addProperty("word", "invalid");
             }
-        } else {
-            responseJson.addProperty("success",false);
-            responseJson.addProperty("hotelId","invalid");
+            response.getWriter().println(responseJson);
+            out.flush();
         }
-        out.println(responseJson);
-        out.flush();
-
     }
 }
