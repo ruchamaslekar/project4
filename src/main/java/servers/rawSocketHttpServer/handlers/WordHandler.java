3009c9397eb2462d43b45d25a99da721e736ee9e
ruchamaslekar <ruchamaslekar@gmail.com>
2023-11-14 13:24:00 -0800

    Modified WordHandler class so that it can provide a response in json format

--- src/main/java/servers/rawSocketHttpServer/handlers/WordHandler.java
+++ src/main/java/servers/rawSocketHttpServer/handlers/WordHandler.java
@@ -1,5 +1,7 @@
 package servers.rawSocketHttpServer.handlers;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import invertedIndexData.ReviewFrequency;
 import invertedIndexData.ThreadSafeInvertedIndex;
 import servers.rawSocketHttpServer.HttpHandler;
@@ -7,21 +9,47 @@ import servers.rawSocketHttpServer.HttpRequest;
 import servers.rawSocketHttpServer.HttpResponse;
 import java.util.Set;
 
+
 public class WordHandler implements HttpHandler {
     private ThreadSafeInvertedIndex threadSafeInvertedIndex;
     @Override
     public void processRequest(HttpRequest request, HttpResponse response) {
-        if (request.getQueryParameters().containsKey("word")) {
-            response.setStatusCode(200, "HTTP/1.1 200 OK" + System.lineSeparator());
-            response.setContentType("ContentType: application/json" + System.lineSeparator() + System.lineSeparator());
+        JsonObject responseJson = new JsonObject();
+        if(request.getQueryParameters().get("word") == null || request.getQueryParameters().get("word").isEmpty()) {
+            responseJson.addProperty("word", "invalid");
+            responseJson.addProperty("success", false);
+            response.sendPageNotFoundResponse(responseJson.toString());
+        }else if(request.getQueryParameters().get("num") == null || request.getQueryParameters().get("num").isEmpty()) {
+            responseJson.addProperty("num", "invalid");
+            responseJson.addProperty("success", false);
+            response.sendPageNotFoundResponse(responseJson.toString());
+        }else {
             String word = request.getQueryParameters().get("word");
             int numOfReviews = (Integer.parseInt(request.getQueryParameters().get("num"))) ;
-            if (word == null) {
-                word = "-1";
-            }
-            else{
-                Set<ReviewFrequency> invertedIndexResponse = threadSafeInvertedIndex.searchByWord(word,numOfReviews);
-                response.sendInvertedIndexResponse(invertedIndexResponse);
+            JsonArray array = new JsonArray();
+            Set<ReviewFrequency> invertedIndexResponse = threadSafeInvertedIndex.searchByWord(word,numOfReviews);
+            if (invertedIndexResponse != null) {
+                ReviewFrequency[] setArray = invertedIndexResponse.toArray(new ReviewFrequency[0]);
+                responseJson.addProperty("word", word);
+                for(ReviewFrequency reviewFrequency : setArray) {
+                    if (reviewFrequency != null) {
+                        JsonObject reviewJson = new JsonObject();
+                        reviewJson.addProperty("reviewId", reviewFrequency.getReview().getReviewId());
+                        reviewJson.addProperty("title", reviewFrequency.getReview().getTitle());
+                        reviewJson.addProperty("user", reviewFrequency.getReview().getUserNickname());
+                        reviewJson.addProperty("reviewText", reviewFrequency.getReview().getReviewText());
+                        reviewJson.addProperty("date", reviewFrequency.getReview().getDate().toString());
+                        array.add(reviewJson);
+                        responseJson.add("reviews", array);
+                    }
+                }
+                responseJson.addProperty("success", true);
+                System.out.println(invertedIndexResponse.toString());
+                response.sendResponse(responseJson.toString());
+            } else {
+                responseJson.addProperty("success", false);
+                responseJson.addProperty("word", "invalid");
+//                response.sendPageNotFoundResponse(responseJson.toString());
             }
         }
     }
