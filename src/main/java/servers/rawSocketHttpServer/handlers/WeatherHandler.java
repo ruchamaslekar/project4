11e810145dbef9a0e52b5224c8fbafb2b10cf896
ruchamaslekar <ruchamaslekar@gmail.com>
2023-11-15 11:38:55 -0800

    Modified processRequest() method in WeatherHandler class by calling getWeatherDataInJSONFormat() method to reduce the code in processRequest() method

--- src/main/java/servers/rawSocketHttpServer/handlers/WeatherHandler.java
+++ src/main/java/servers/rawSocketHttpServer/handlers/WeatherHandler.java
@@ -1,14 +1,10 @@
 package servers.rawSocketHttpServer.handlers;
 
 import com.google.gson.JsonObject;
-import hotelData.Hotel;
 import hotelData.ThreadSafeHotelDetails;
 import servers.rawSocketHttpServer.HttpHandler;
 import servers.rawSocketHttpServer.HttpRequest;
 import servers.rawSocketHttpServer.HttpResponse;
-import weatherData.WeatherFetcher;
-
-import java.io.IOException;
 
 public class WeatherHandler implements HttpHandler {
     private ThreadSafeHotelDetails threadSafeHotelDetails;
@@ -19,28 +15,11 @@ public class WeatherHandler implements HttpHandler {
             responseJson.addProperty("hotelId", "invalid");
             responseJson.addProperty("success", false);
             response.sendPageNotFoundResponse(responseJson.toString());
-        }
-        else {
+        }else {
             String hotelId = request.getQueryParameters().get("hotelId");
-            Hotel hotelInfoResponse = threadSafeHotelDetails.getHotel(hotelId);
-            if(hotelInfoResponse != null) {
-                responseJson.addProperty("hotelId", hotelId);
-                responseJson.addProperty("name", hotelInfoResponse.getHotelName());
-                responseJson.addProperty("addr", hotelInfoResponse.getAddress());
-                responseJson.addProperty("city", hotelInfoResponse.getCity());
-                responseJson.addProperty("state", hotelInfoResponse.getState());
-                responseJson.addProperty("lat", hotelInfoResponse.getLatitude());
-                responseJson.addProperty("lng", hotelInfoResponse.getLongitude());
-                JsonObject weatherDetails = WeatherFetcher.fetch("api.open-meteo.com","/v1/forecast?latitude="+hotelInfoResponse.getLatitude()+"&longitude="+hotelInfoResponse.getLongitude()+"&current_weather=true");
-                responseJson.add("weather", weatherDetails);
-                responseJson.addProperty("success", true);
-                response.sendResponse(responseJson.toString());
-            } else {
-                responseJson.addProperty("hotelId", "invalid");
-                responseJson.addProperty("success", false);
-                response.sendPageNotFoundResponse(responseJson.toString());
-            }
-            System.out.println(hotelInfoResponse.toString());
+            responseJson = threadSafeHotelDetails.getWeatherDataInJSONFormat(hotelId);
+            response.sendResponse(responseJson.toString());
+            System.out.println(responseJson);
         }
     }
     @Override
