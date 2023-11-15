eecc454d663aa4953f498e70c9a46286a7b8bb12
ruchamaslekar <ruchamaslekar@gmail.com>
2023-11-15 11:35:32 -0800

    Modified processRequest() method in HotelHandler class by calling getHotelInJSONFormat() method to reduce the code in processRequest() method

--- src/main/java/servers/rawSocketHttpServer/handlers/HotelHandler.java
+++ src/main/java/servers/rawSocketHttpServer/handlers/HotelHandler.java
@@ -16,26 +16,11 @@ public class HotelHandler implements HttpHandler {
             responseJson.addProperty("hotelId", "invalid");
             responseJson.addProperty("success", false);
             response.sendPageNotFoundResponse(responseJson.toString());
-        }
-        else {
+        } else {
             String hotelId = request.getQueryParameters().get("hotelId");
-            Hotel hotelInfoResponse = threadSafeHotelDetails.getHotel(hotelId);
-            if(hotelInfoResponse != null) {
-                responseJson.addProperty("hotelId", hotelId);
-                responseJson.addProperty("name", hotelInfoResponse.getHotelName());
-                responseJson.addProperty("addr", hotelInfoResponse.getAddress());
-                responseJson.addProperty("city", hotelInfoResponse.getCity());
-                responseJson.addProperty("state", hotelInfoResponse.getState());
-                responseJson.addProperty("lat", hotelInfoResponse.getLatitude());
-                responseJson.addProperty("lng", hotelInfoResponse.getLongitude());
-                responseJson.addProperty("success", true);
-                response.sendResponse(responseJson.toString());
-            } else {
-                responseJson.addProperty("hotelId", "invalid");
-                responseJson.addProperty("success", false);
-                response.sendPageNotFoundResponse(responseJson.toString());
-            }
-            System.out.println(hotelInfoResponse.toString());
+            responseJson = threadSafeHotelDetails.getHotelInJSONFormat(hotelId);
+            response.sendResponse(responseJson.toString());
+            System.out.println(responseJson.toString());
         }
     }
     @Override
