58794466f1d8af99a27c997e86c7c91f8726a7cc
ruchamaslekar <ruchamaslekar@gmail.com>
2023-11-15 11:37:03 -0800

    Modified doGet() method in InvertedIndexServletServlet class by calling getInvertedIndexInJSONFormat() method to reduce the code in doGet() method

--- src/main/java/servers/jettyServer/InvertedIndexServlet.java
+++ src/main/java/servers/jettyServer/InvertedIndexServlet.java
@@ -17,46 +17,24 @@ public class InvertedIndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
         response.setStatus(HttpServletResponse.SC_OK);
         JsonObject responseJson = new JsonObject();
         PrintWriter out = response.getWriter();
+        InvertedIndex invertedIndex = (InvertedIndex) getServletContext().getAttribute("word");
         String word = request.getParameter("word");
         word = StringEscapeUtils.escapeHtml4(word);
-        if (word == null || word.isEmpty()) {
+        if(word == null || word.isEmpty()) {
             responseJson.addProperty("success", false);
             responseJson.addProperty("word", "invalid");
-            response.getWriter().println(responseJson);
-            out.flush();
-        } else if (request.getParameter("num") == null || request.getParameter("num").isEmpty()) {
+        }else if(request.getParameter("num") == null || request.getParameter("num").isEmpty()) {
             responseJson.addProperty("success", false);
             responseJson.addProperty("num", "invalid");
-            response.getWriter().println(responseJson);
-            out.flush();
-        } else {
+        }else {
             int numOfReviews = (Integer.parseInt(request.getParameter("num")));
-            JsonArray array = new JsonArray();
-            InvertedIndex invertedIndex = (InvertedIndex) getServletContext().getAttribute("word");
-            Set<ReviewFrequency> reviews = invertedIndex.searchByWord(word, numOfReviews);
-            if (reviews != null && !reviews.isEmpty()) {
-                ReviewFrequency[] setArray = reviews.toArray(new ReviewFrequency[0]);
-                responseJson.addProperty("word", word);
-                for(ReviewFrequency reviewFrequency : setArray) {
-                    JsonObject reviewJson = new JsonObject();
-                    reviewJson.addProperty("reviewId", reviewFrequency.getReview().getReviewId());
-                    reviewJson.addProperty("title", reviewFrequency.getReview().getTitle());
-                    reviewJson.addProperty("user", reviewFrequency.getReview().getUserNickname());
-                    reviewJson.addProperty("reviewText", reviewFrequency.getReview().getReviewText());
-                    reviewJson.addProperty("date", reviewFrequency.getReview().getDate().toString());
-                    array.add(reviewJson);
-                    responseJson.add("reviews", array);
-                }
-                responseJson.addProperty("success", true);
-            } else {
-                responseJson.addProperty("success", false);
-                responseJson.addProperty("word", "invalid");
-            }
-            response.getWriter().println(responseJson);
-            out.flush();
+            responseJson = invertedIndex.getInvertedIndexInJSONFormat(word,numOfReviews);
         }
+        response.getWriter().println(responseJson);
+        out.flush();
     }
 }
