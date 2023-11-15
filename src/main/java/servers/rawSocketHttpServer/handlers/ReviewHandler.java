7aeb2eb82a4f10e634192c9fefad182adac68bcc
ruchamaslekar <ruchamaslekar@gmail.com>
2023-11-15 11:35:57 -0800

    Modified processRequest() method in ReviewHandler class by calling getReviewsInJSONFormat() method to reduce the code in processRequest() method

--- src/main/java/servers/rawSocketHttpServer/handlers/ReviewHandler.java
+++ src/main/java/servers/rawSocketHttpServer/handlers/ReviewHandler.java
@@ -1,57 +1,30 @@
 package servers.rawSocketHttpServer.handlers;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import reviewData.Review;
 import reviewData.ThreadSafeReviewDetails;
 import servers.rawSocketHttpServer.HttpHandler;
 import servers.rawSocketHttpServer.HttpRequest;
 import servers.rawSocketHttpServer.HttpResponse;
-import java.util.List;
 
 public class ReviewHandler implements HttpHandler {
     private ThreadSafeReviewDetails threadSafeReviewDetails;
     @Override
     public void processRequest(HttpRequest request, HttpResponse response) {
         JsonObject responseJson = new JsonObject();
-        if (request.getQueryParameters().get("hotelId") == null || request.getQueryParameters().get("hotelId").isEmpty()) {
+        if(request.getQueryParameters().get("hotelId") == null || request.getQueryParameters().get("hotelId").isEmpty()) {
             responseJson.addProperty("hotelId", "invalid");
             responseJson.addProperty("success", false);
             response.sendPageNotFoundResponse(responseJson.toString());
-        } else if(request.getQueryParameters().get("num") == null || request.getQueryParameters().get("num").isEmpty()){
+        }else if(request.getQueryParameters().get("num") == null || request.getQueryParameters().get("num").isEmpty()){
             responseJson.addProperty("num", "invalid");
             responseJson.addProperty("success", false);
             response.sendPageNotFoundResponse(responseJson.toString());
-        } else {
+        }else {
             String hotelId = request.getQueryParameters().get("hotelId");
             int numOfReviews = (Integer.parseInt(request.getQueryParameters().get("num")));
-            List<Review> reviewsResponse = threadSafeReviewDetails.getReviews(hotelId, numOfReviews);
-            if (reviewsResponse.isEmpty()) {
-                responseJson.addProperty("hotelId", "invalid");
-                responseJson.addProperty("success", false);
-                response.sendPageNotFoundResponse(responseJson.toString());
-            } else {
-                JsonArray array = new JsonArray();
-                responseJson.addProperty("hotelId", hotelId);
-                for (Review review : reviewsResponse) {
-                    if (review != null) {
-                        JsonObject reviewJson = new JsonObject();
-                        reviewJson.addProperty("user", review.getUserNickname());
-                        reviewJson.addProperty("title", review.getTitle());
-                        reviewJson.addProperty("reviewId", review.getReviewId());
-                        reviewJson.addProperty("reviewText", review.getReviewText());
-                        array.add(reviewJson);
-                        responseJson.add("reviews", array);
-                    } else {
-                        responseJson.addProperty("success", false);
-                        responseJson.addProperty("hotelId", "invalid");
-                        response.sendPageNotFoundResponse(response.toString());
-                    }
-                }
-                responseJson.addProperty("success", true);
-                System.out.println(reviewsResponse.toString());
-                response.sendResponse(responseJson.toString());
-            }
+            responseJson = threadSafeReviewDetails.getReviewsInJSONFormat(hotelId,numOfReviews);
+            System.out.println(responseJson.toString());
+            response.sendResponse(responseJson.toString());
         }
     }
     @Override
