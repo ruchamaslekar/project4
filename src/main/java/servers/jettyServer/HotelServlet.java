3b758e603b764cb9b48883a4cd97c1e41beb63bd
ruchamaslekar <ruchamaslekar@gmail.com>
2023-11-15 11:34:49 -0800

    Modified doGet() method by calling getHotelInJSONFormat() method to reduce the code in doGet() method

--- src/main/java/servers/jettyServer/HotelServlet.java
+++ src/main/java/servers/jettyServer/HotelServlet.java
@@ -1,12 +1,12 @@
 package servers.jettyServer;
 
 import com.google.gson.JsonObject;
-import hotelData.Hotel;
 import hotelData.HotelDetails;
 import org.apache.commons.text.StringEscapeUtils;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class HotelServlet extends HttpServlet {
@@ -15,6 +15,7 @@ public class HotelServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         try{
             response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
             response.setStatus(HttpServletResponse.SC_OK);
             PrintWriter out = response.getWriter();
             JsonObject responseJson = new JsonObject();
@@ -24,27 +25,13 @@ public class HotelServlet extends HttpServlet {
             if(hotelId == null){
                 responseJson.addProperty("hotelId", "invalid");
                 responseJson.addProperty("success", false);
-            }
-            else {
-                Hotel hotel  = hotelDetails.getHotel(hotelId);
-                if(hotel != null) {
-                    responseJson.addProperty("hotelId", hotelId);
-                    responseJson.addProperty("name", hotel.getHotelName());
-                    responseJson.addProperty("addr", hotel.getAddress());
-                    responseJson.addProperty("city", hotel.getCity());
-                    responseJson.addProperty("state", hotel.getState());
-                    responseJson.addProperty("lat", hotel.getLatitude());
-                    responseJson.addProperty("lng", hotel.getLongitude());
-                    responseJson.addProperty("success", true);
-                } else {
-                    responseJson.addProperty("hotelId", "invalid");
-                    responseJson.addProperty("success", false);
-                }
+            } else {
+                responseJson = hotelDetails.getHotelInJSONFormat(hotelId);
             }
             response.getWriter().println(responseJson);
             out.flush();
-        } catch(Exception e) {
-            System.out.println("Error"+e);
+            } catch (IOException ex) {
+                System.out.println("Error"+ex);
         }
     }
 }
